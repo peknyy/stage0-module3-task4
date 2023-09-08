c298ebd169351ec2857386da016ad005c7516244
peknyy <lukcymr3@gmail.com>
2023-09-08 15:57:54 +0600

    Done

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int[] array = {99,104,97,114};
+        for (int i = 0; i < 4; i++) {
+            System.out.println((char) array[i]);
+        }
     }
 }
